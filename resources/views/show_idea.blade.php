a34704c56dd794361407a438dd39ab0725049e17
ridhofh <ridhofahrorozihasyim@gmail.com>
2017-03-23 16:56:10 +0700

    rev button skip func

--- resources/views/show_idea.blade.php
+++ resources/views/show_idea.blade.php
@@ -17,10 +17,14 @@
                     <div class="panel-body">
                         <p>{{$idea->description}}</p>
                         <a href="#" class="btn btn-success btn-lg col-xs-12" id="btn-like" style="margin-bottom:10px">Like</a>
-                        <a href="#" class="btn btn-default btn-lg col-xs-12">Skip</a>
+                        <a href="#" class="btn btn-default btn-lg col-xs-12" id="btn-skip" >Skip</a>
                     </div>
 
                     <form id="form-like" action="{{ route("idea.like") }}" method="POST" style="display: none">
+                        {!! csrf_field() !!}
+                        <input type="text" name="id" value="{{$idea->id}}">
+                    </form>
+                    <form id="form-skip" action="{{ route("idea.skip") }}" method="POST" style="display: none">
                         {!! csrf_field() !!}
                         <input type="text" name="id" value="{{$idea->id}}">
                     </form>
@@ -35,6 +39,12 @@
     $(function (e) {
         $("#btn-like").click(function (e) {
             $("#form-like").submit();
+        })
+    })
+
+    $(function (e) {
+        $("#btn-skip").click(function (e) {
+            $("#form-skip").submit();
         })
     })
 </script>
--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,6 @@ Route::get('/home', 'HomeController@index');
 Route::get('/idea/{id}', 'IdeaController@show')->name('idea.show');
 Route::post('/idea/store', 'IdeaController@store')->name('idea.store');
 Route::post('/idea/like/', 'IdeaController@like')->name('idea.like');
+Route::post('/idea/skip/', 'IdeaController@skip')->name('idea.skip');
 
 Route::get('/admin/dashboard', 'AdminController@dashboard')->name('admin.dashboard');
