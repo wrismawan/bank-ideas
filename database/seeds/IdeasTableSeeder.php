25c66f46c609eba03a8cd38f415f3365bdd68e9e
Wahyu Rismawan <wahyu@badr-interactive.com>
2017-03-23 16:32:35 +0700

    create ideas seeder

---
+++ database/seeds/IdeasTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class IdeasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('ideas')->insert([
+            [
+                'name' => "reconnecting",
+                'description' => "aplikasi mobile yang membantu kaum urban dalam mempermudah kegiatan networking dan silaturahim mereka dengan sistem manajemen kontak, pesan, dan penjadwalan silaturahim otomatis",
+                'like' => 0,
+                'skip' => 0,
+                'viewed' => 0,
+            ],
+            [
+                'name' => "SakuNelayan",
+                'description' => "platform pasar online yang mempertemukan pembeli dengan nelayan dalam bentuk permodalan (jual beli salam)",
+                'like' => 0,
+                'skip' => 0,
+                'viewed' => 0,
+            ],
+            [
+                'name' => "youngspeak",
+                'description' => "aplikasi mobile yang menjadi channel kaum muda inspiratif untuk sharing pemikiran/ide/ilmu dalam bentuk video dan suara, sehingga bisa mendapatkan audience dengan konsep social media",
+                'like' => 0,
+                'skip' => 0,
+                'viewed' => 0,
+            ],
+        ]);
+    }
+}
