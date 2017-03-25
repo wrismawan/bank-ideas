b2d8ef79b1caa2cd35d858830cc0810936a8aa68
Wahyu Rismawan <wahyu@badr-interactive.com>
2017-03-25 20:40:24 +0700

    welcome message after login

--- resources/views/show_idea.blade.php
+++ resources/views/show_idea.blade.php
@@ -12,6 +12,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
+                @if (session('message'))
+                <div class="alert alert-success alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <strong>HOW:</strong> Pilih <strong>"Yep!"</strong> jika kamu suka, dan <strong>"Nope"</strong> jika kamu ingin langsung melihat ide lainnya.
+                </div>
+                @endif
+
                 <div class="panel panel-default">
                     <div class="panel-heading"><h3 class="text-center">{{$idea->name}}</h3></div>
                     <div class="panel-body">
